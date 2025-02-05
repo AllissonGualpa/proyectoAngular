1d527193cfc20af7943b53bb7e2c750f81466a19
AllissonGualpa <allissongualpa@gmail.com>
2025-02-05 10:06:17 -0500

    Creación de carpeta shared/contacto, Agregar un atributo al controlador de contactos, y en la vista de contactos se muestra el valor del atributo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactoComponent } from './shared/contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
