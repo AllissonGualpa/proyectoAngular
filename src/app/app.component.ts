790b8ed4d2063e06913dd0b708d928389f6b04d1
AllissonGualpa <allissongualpa@gmail.com>
2025-02-06 10:47:30 -0500

    Se realizó un componente footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import { AlbumComponent } from './shared/album/album.component';
 import { VentanasComponent } from './shared/ventanas/ventanas.component';
 import { RecursosService } from './servicios/recursos.service';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './shared/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ContactoComponent, AlbumComponent, VentanasComponent, HttpClientModule],
+  imports: [RouterOutlet,ContactoComponent, AlbumComponent, VentanasComponent, HttpClientModule, FooterComponent],
   providers: [RecursosService], //se lo pone como proveedor porque es un servicio.
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
